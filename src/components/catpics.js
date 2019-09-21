fd6d3b0de47fd8cc9c5986893c45bc7cc1c4f20b
Erik Cribley <erikcribley@gmail.com>
2019-09-21 19:07:40 -0400

    images appear, yay!

---
+++ src/components/catpics.js
@@ -0,0 +1,59 @@
+const cats = [
+  {
+    id: 1,
+    src: './images/cat1.jpg',
+    clicked: false
+  },
+  {
+    id: 2,
+    src: './images/cat2.jpg',
+    clicked: false
+  },
+  {
+    id: 3,
+    src: './images/cat3.jpg',
+    clicked: false
+  },
+  {
+    id: 4,
+    src: './images/cat4.jpg',
+    clicked: false
+  },
+  {
+    id: 5,
+    src: './images/cat5.jpg',
+    clicked: false
+  },
+  {
+    id: 6,
+    src: './images/cat6.jpg',
+    clicked: false
+  },
+  {
+    id: 7,
+    src: './images/cat7.jpg',
+    clicked: false
+  },
+  {
+    id: 8,
+    src: './images/cat8.jpg',
+    clicked: false
+  },
+  {
+    id: 9,
+    src: './images/cat9.jpg',
+    clicked: false
+  },
+]
+
+// function List (props) {
+//   return (
+//     <div>
+//       {props.cats.map(item => (
+//         <img scr={item.image} key={item.id} alt="kitty kat"></img>
+//       ))}
+//     </div>
+//   )
+// }
+
+export default cats
--- src/components/game.js
+++ src/components/game.js
@@ -1,2 +1,12 @@
 import React from 'react'
+import cats from './catpics'
 
+class Game extends React.Component {
+  render() {
+    return (
+      cats.map(({id, src}) => <img key={id} src={src} alt ="kitty kat" />)
+    )
+  }
+} 
+
+export default Game
