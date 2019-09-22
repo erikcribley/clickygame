d6c1f30dfd3be191da4307d3db2295779578f93a
Erik Cribley <erikcribley@gmail.com>
2019-09-22 00:47:57 -0400

    state change, randomize array

--- src/components/game.js
+++ src/components/game.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import cats from './catpics'
 
+const shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -=1
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+    return array
+}
+
 class Game extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: true
+    })
+    shuffle(cats)
+  }
+  
   render() {
     return (
-      cats.map(({id, src}) => <img key={id} src={src} alt ="kitty kat" />)
+      cats.map(({id, src}) => <img key={id} src={src} alt ="kitty kat" onClick={this.handleClick} />)
     )
   }
-} 
+}
 
 export default Game
