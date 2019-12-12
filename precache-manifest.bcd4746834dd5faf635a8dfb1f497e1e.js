cec212d60a5ef2b13556180c3c8a01b94b4ccfc4
Erik Cribley <erikcribley@gmail.com>
2019-12-12 01:55:11 -0500

    Updates

---
+++ precache-manifest.bcd4746834dd5faf635a8dfb1f497e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abf6be58df14f0aa5af1e3a9b89e115",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "a3a2ffa9fe65b28b7d1a",
+    "url": "/clickygame/static/css/main.ee493363.chunk.css"
+  },
+  {
+    "revision": "00bf26148eef635e7696",
+    "url": "/clickygame/static/js/2.26314e3b.chunk.js"
+  },
+  {
+    "revision": "a3a2ffa9fe65b28b7d1a",
+    "url": "/clickygame/static/js/main.06d923cc.chunk.js"
+  },
+  {
+    "revision": "9a2bb6034f7f30e15eb9",
+    "url": "/clickygame/static/js/runtime~main.c2253ab0.js"
+  }
+]);
